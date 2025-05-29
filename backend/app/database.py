adaa0f4e999e64ede0d0a726d2b2c17952bad24b
Aissa Laribi <aissalaribi@yahoo.fr>
2025-05-29 17:33:17 +0100

    db engine echo set to false for better user experience for quizzes taken  on CLI

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,7 @@ load_dotenv(".env")  # Load variables from .env file
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
