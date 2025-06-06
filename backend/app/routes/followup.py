d1028ef95d9a76313ca697047902af82eae2f183
Aissa Laribi <aissalaribi@yahoo.fr>
2025-06-07 00:27:35 +0100

    Removing duplicate followups bug

--- backend/app/routes/followup.py
+++ backend/app/routes/followup.py
@@ -11,8 +11,18 @@ router = APIRouter()
 
 @router.post("/users/{user_id}/followups/{module_id}/{quiz_id}")
 async def post_follow_up_quiz(user_id: int,module_id: int, quiz_id: int, followup: FollowupCreate, due_date: datetime, db: AsyncSession = Depends(get_db)):
-    followup = Followup(followup_due_date = due_date, user_id = user_id, module_id = module_id, quiz_id = quiz_id)
-    db.add(followup)
+    result = await db.execute(
+        select(Followup).where(
+            Followup.user_id == user_id,
+            Followup.module_id == module_id,
+            Followup.quiz_id == quiz_id
+        )
+    )
+    followup = result.scalar_one_or_none()
+    if followup:
+        followup.followup_due_date = due_date
+    else:
+        followup = Followup(followup_due_date = due_date, user_id = user_id, module_id = module_id, quiz_id = quiz_id)
     await db.commit()
     await db.refresh(followup)
 
