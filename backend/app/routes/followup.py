7232a24cd5e0147884c33dc9de43d00a522cb594
Aissa Laribi <aissalaribi@yahoo.fr>
2025-06-08 20:12:30 +0100

    Implementation GET followups returns by ascending next_due_date

--- backend/app/routes/followup.py
+++ backend/app/routes/followup.py
@@ -1,41 +1,21 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.models import User, Module, Quiz, Question, Followup
-from app.schemas import FollowupCreate
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
-from datetime import datetime
 
 router = APIRouter()
-
-
-@router.post("/users/{user_id}/followups/{module_id}/{quiz_id}")
-async def post_follow_up_quiz(user_id: int,module_id: int, quiz_id: int, followup: FollowupCreate, due_date: datetime, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(
-        select(Followup).where(
-            Followup.user_id == user_id,
-            Followup.module_id == module_id,
-            Followup.quiz_id == quiz_id
-        )
-    )
-    followup = result.scalar_one_or_none()
-    if followup:
-        followup.followup_due_date = due_date
-    else:
-        followup = Followup(followup_due_date = due_date, user_id = user_id, module_id = module_id, quiz_id = quiz_id)
-    await db.commit()
-    await db.refresh(followup)
 
 
 
 @router.get("/users/{user_id}/followups")
 async def get_follow_up_quizzes(user_id: int, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(Followup).where(Followup.user_id == user_id))
+    result = await db.execute(select(Followup).where(Followup.user_id == user_id).order_by(Followup.followup_due_date))
     followups = result.scalars().all()
     return followups
 
 @router.get("/users/{user_id}/followups/{module_id}/{quiz_id}")
 async def get_follow_up_quiz(user_id: int,module_id: int, quiz_id: int, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(Followup).where(Followup.user_id == user_id).where(Followup.module_id == module_id).where(Followup.quiz_id == quiz_id))
+    result = await db.execute(select(Followup).where(Followup.user_id == user_id).where(Followup.module_id == module_id).where(Followup.quiz_id == quiz_id).order_by(Followup.followup_due_date))
     followup = result.scalars().all()
     return followup
