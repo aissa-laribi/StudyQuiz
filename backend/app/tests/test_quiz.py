9045e1cff56a0f55f8a3d0bb7dfae9caa81aa10c
Aissa Laribi <aissalaribi@yahoo.fr>
2026-05-25 14:22:23 +0100

    Add integration tests for getting the quizzes not attempted

--- backend/app/tests/test_quiz.py
+++ backend/app/tests/test_quiz.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 from passlib.context import CryptContext
 from httpx import AsyncClient, ASGITransport
@@ -475,7 +477,7 @@ async def test_get_quizzes(async_app_client):
     assert response.json()[2]['quiz_name'] == "Quiz 3"
 
 @pytest.mark.anyio
-async def test_get_quizzes(async_app_client):
+async def test_get_quizzes_not_attempted(async_app_client):
     form_data = (
         "grant_type=password&username=testuser1"
         "&password=StrongPwd1234,,,,tewfw4g"
@@ -520,9 +522,149 @@ async def test_get_quizzes(async_app_client):
     response = await async_app_client.post(f"/users/{user_id}/modules/{module_id}/quizzes/batch-create/", json=data, headers=headers)
     assert response.status_code == 200
     name = "Module 1"
-    response = await async_app_client.get(f"/users/me/modules/{name}/quizzes/",headers=headers)
+    response = await async_app_client.get(f"/users/me/quizzes/new",headers=headers)
     assert response.status_code == 200
     assert len(response.json()) == 3
     assert response.json()[0]['quiz_name'] == "Quiz 1"
     assert response.json()[1]['quiz_name'] == "Quiz 2"
     assert response.json()[2]['quiz_name'] == "Quiz 3"
+
+    data = {
+        "name": "Module 2"
+    }
+    response = await async_app_client.post(f"/users/{user_id}/modules/", json=data, headers=headers)
+    assert response.status_code == 200
+    response = await async_app_client.post(f"/users/{user_id}/modules/", json=data, headers=headers)
+    assert response.status_code == 200
+    response = await async_app_client.get(f"/users/{user_id}/modules/", headers=headers)
+    assert response.status_code == 200
+    module_id = response.json()[0]['id']
+    assert response.json()[1]['module_name'] == 'Module 2'
+    data = {
+        "data": [
+        {
+            "name": "Quiz 4"
+        },
+        {
+            "name": "Quiz 5"
+        },
+        {
+            "name": "Quiz 6"
+        },
+        ]
+    }
+    response = await async_app_client.post(f"/users/{user_id}/modules/{module_id}/quizzes/batch-create/", json=data, headers=headers)
+    assert response.status_code == 200
+    response = await async_app_client.get(f"/users/me/quizzes/new",headers=headers)
+    assert response.status_code == 200
+    assert len(response.json()) == 6
+    assert response.json()[0]['quiz_name'] == "Quiz 1"
+    assert response.json()[1]['quiz_name'] == "Quiz 2"
+    assert response.json()[2]['quiz_name'] == "Quiz 3"
+    assert response.json()[3]['quiz_name'] == "Quiz 4"
+    assert response.json()[4]['quiz_name'] == "Quiz 5"
+    assert response.json()[5]['quiz_name'] == "Quiz 6"
+
+    #Now attempt quiz 1
+    quiz_id = response.json()[0]['id']
+    quiz_data = {
+    "questions": [
+        {
+            "name": "Capital of Ireland?",
+            "answers": [
+                {"name": "Dublin", "correct": True},
+                {"name": "Bamako", "correct": False},
+            ],
+        },
+        {
+            "name": "Capital of UK?",
+            "answers": [
+                {"name": "London", "correct": True},
+                {"name": "Berlin", "correct": False},
+            ],
+        },
+        {
+            "name": "Capital of France?",
+            "answers": [
+                {"name": "Paris", "correct": True},
+                {"name": "Lyon", "correct": False},
+            ],
+        },
+    ]
+}
+    response = await async_app_client.patch(f"/users/{user_id}/modules/{module_id}/quizzes/{quiz_id}", json=data, headers=headers)
+    assert response.status_code == 200
+    data = {
+    "questions": [
+        {
+            "name": "Capital of Ireland?",
+            "answers": [
+                {"name": "Dublin", "correct": True},
+                {"name": "Bamako", "correct": False},
+            ],
+        },
+        {
+            "name": "Capital of UK?",
+            "answers": [
+                {"name": "London", "correct": True},
+                {"name": "Berlin", "correct": False},
+            ],
+        },
+        {
+            "name": "Capital of France?",
+            "answers": [
+                {"name": "Paris", "correct": True},
+                {"name": "Lyon", "correct": False},
+            ],
+        },
+    ]
+}
+    response = await async_app_client.post(f"/users/{user_id}/modules/{module_id}/quizzes/{quiz_id}/questions/batch-create", json=data, headers=headers)
+    assert response.status_code == 200
+
+    module_name = "Module 1"
+    quiz_name = "Quiz 1"
+    response = await async_app_client.get(f"/users/me/modules/{module_name}/quizzes/{quiz_name}/questions/attempts/shuffled-questions", headers=headers)
+    assert response.status_code == 200
+    questions = []
+    for question in response.json():
+        answers = await async_app_client.get(f"/users/me/modules/{module_name}/quizzes/{quiz_name}/questions/{question['id']}/answers", headers=headers)
+        questions.append([question['id'], answers.json()])
+          
+    data = {
+        "created_at": str(datetime.now()),
+        "answers": [
+            {
+                "question_id": questions[0][0],
+                "answer_id": list(filter(lambda x:x['answer_correct'] == True,questions[0][1] ))[0]['id']
+                },
+            {
+                "question_id": questions[1][0],
+                "answer_id": list(filter(lambda x:x['answer_correct'] == True,questions[1][1] ))[0]['id']
+                },      
+            {
+                "question_id": questions[1][0],
+                "answer_id": list(filter(lambda x:x['answer_correct'] == False,questions[2][1] ))[0]['id']
+            }      
+            ]
+            }
+    response = await async_app_client.post(f"/users/me/modules/{module_name}/quizzes/{quiz_name}/attempts/",json=data,headers=headers)
+    assert response.status_code == 200
+    assert response.json()['score'] == 67
+    assert response.json()['correct_answers'] == 2
+    assert response.json()['total_questions'] == 3
+    assert response.json()['repetitions'] == 1
+    assert response.json()['interval'] == 1
+    assert response.json()['ease_factor'] == 2.18
+
+    #Now check quiz 1 is not a not attampted quiz
+    response = await async_app_client.get(f"/users/me/quizzes/new",headers=headers)
+    assert response.status_code == 200
+    assert len(response.json()) == 5
+    print(response.json())
+    quiz_names = []
+    for quiz in response.json():
+        quiz_names.append(quiz['quiz_name'])
+    print(quiz_names)
+    assert 'Quiz 2' in quiz_names
+    assert 'Quiz 1' not in quiz_names
