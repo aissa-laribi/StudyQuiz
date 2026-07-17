9e78df067c971cc8b2c76b592158fe746d1f4901
Aissa Laribi <aissalaribi@yahoo.fr>
2026-07-17 21:39:11 +0100

    Add 10 seconds to login timeout

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -33,7 +33,7 @@
   const password = formData.get("password");
 
   const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), 10000);
+  const timeoutId = setTimeout(() => controller.abort(), 20000);
 
   try {
     const res = await fetch(`${apiURL}/users/token`, {
