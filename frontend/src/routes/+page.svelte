8b5cdd7f5b55e5cad66ed1685e333f1ffc1a19b5
Aissa Laribi <aissalaribi@yahoo.fr>
2026-08-03 14:39:47 +0100

    Remove user plans prod (not ready yet)

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -482,6 +482,7 @@
     {/if}
   </div>
   </div>
+<!--  
   <div id="plans-grid">
   <div id="plans-grid-inner">
     <div class="plan-col">
@@ -512,7 +513,7 @@
     <p>StudyQuiz plans are not open yet. Join a waiting list to be notified when access becomes available.</p>
   </div>
   </div>
-  
+-->  
 </main>
   <div id="sidebar1"></div>
   <div id="sidebar2"></div>
