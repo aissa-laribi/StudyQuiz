1b59e61463b0896266a64bca8e92e343fa636d1c
Aissa Laribi <aissalaribi@yahoo.fr>
2026-08-09 19:41:01 +0100

    Fix sitemap canonical URL

--- frontend/src/routes/sitemap.xml/+server.js
+++ frontend/src/routes/sitemap.xml/+server.js
@@ -5,7 +5,7 @@ export async function GET() {
 		`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url>
-		<loc>https://www.studyquiz.co/</loc>ß
+		<loc>https://www.studyquiz.co/</loc>
 	</url>
 </urlset>`,
 		{
